ecd17aa468339793ab4a388678c6c5256a6d62c7
Hatsunmikk <debos2003@gmail.com>
2024-12-22 12:19:41 +0530

    Add an operator function to tie together the mathematical functions with the numbers and the chosen operator

--- logic.js
+++ logic.js
@@ -17,3 +17,26 @@ function divide (a, b) {
     if(b === 0) return "Cannot divide a number by 0.";
     return a / b;
 };
+
+
+function operate (operator, num1, num2) {
+
+    switch(operator){
+
+        case '+':
+            return add(num1, num2);
+
+        case '-':
+            return subtract(num1, num2);
+
+        case 'x':
+            return multiply(num1, num2);
+
+        case '/':
+            return divide(num1, num2);
+
+        default:
+            return null;
+
+    };
+};
